9fd5a38ace87c7bdfd89e0aaaebfce9494ec9cf6
divyanshu483 <divyanshuaggarwal483@gmail.com>
2026-06-12 17:15:49 +0530

    Change HOST variable to point to GitHub redirect script

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ DG='\033[0;38;5;244m'    # Steel Gray
 NC='\033[0m'             # Reset
 
 # --- CONFIG ---
-HOST="run.nobitahost.in"
+HOST="raw.githubusercontent.com/divyanshu483/test1/refs/heads/main/redirecting.sh"
 URL="https://${HOST}"
 NETRC="${HOME}/.netrc"
 IP="65.0.86.121"
